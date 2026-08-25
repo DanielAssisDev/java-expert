f5871fc842a32c545d87aac4087700e30af54fa0
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-08-25 11:00:12 -0300

    Trocando pageRequest por pageable

--- backend/src/main/java/com/daniel/dev/controllers/ProductController.java
+++ backend/src/main/java/com/daniel/dev/controllers/ProductController.java
@@ -6,6 +6,7 @@ import com.daniel.dev.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,13 +21,8 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(
-            @RequestParam(value = "page", defaultValue = "0") Integer page,
-            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer size,
-            @RequestParam(value = "direction", defaultValue = "DESC") String direction,
-            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
-    ){
-        return ResponseEntity.ok(productService.findAll(PageRequest.of(page, size, Sort.Direction.valueOf(direction), orderBy)));
+    public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok(productService.findAll(pageable));
     }
 
     @GetMapping("/{id}")
