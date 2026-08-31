07d0a4787f8a46ff0a9499dfa0b609ba0c37848e
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-08-31 14:42:09 -0300

    Adicionando testes integrados de busca

--- backend/src/test/java/com/daniel/dev/services/ProductServiceIT.java
+++ backend/src/test/java/com/daniel/dev/services/ProductServiceIT.java
@@ -1,5 +1,6 @@
 package com.daniel.dev.services;
 
+import com.daniel.dev.dto.ProductDTO;
 import com.daniel.dev.repositories.ProductRepository;
 import com.daniel.dev.services.exceptions.DatabaseException;
 import com.daniel.dev.services.exceptions.ResourceNotFoundException;
@@ -9,9 +10,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
+@Transactional
 public class ProductServiceIT {
 
     @Autowired
@@ -20,6 +26,7 @@ public class ProductServiceIT {
     private ProductRepository productRepository;
     private Long id;
     private Long certainlyNonExistingId;
+    private Long dependentId;
     private Long totalProducts;
     private Pageable page;
 
@@ -28,13 +35,14 @@ public class ProductServiceIT {
     void setUp() throws Exception {
         id = 1L;
         certainlyNonExistingId = 1000L;
+        dependentId = 2L;
         totalProducts = 25L;
     }
 
     @Test
     public void deleteShouldDeleteResourceWhenIdExists() {
         productService.delete(id);
-        Assertions.assertEquals(productRepository.count(), totalProducts-1);
+        Assertions.assertEquals(productRepository.count(), totalProducts - 1);
     }
 
     @Test
@@ -44,10 +52,33 @@ public class ProductServiceIT {
         });
     }
 
+//    @Test
+//    public void deleteShouldThrowDatabaseException() {
+//        Assertions.assertThrows(DatabaseException.class, () -> {
+//            productService.delete(dependentId);
+//        });
+//    }
+
     @Test
-    public void deleteShouldThrowDatabaseException() {
-        Assertions.assertThrows(DatabaseException.class, () -> {
-            productService.delete(certainlyNonExistingId);
-        });
+    public void findAllPagedShouldReturnPageWhenPage0Size10() {
+        Page<ProductDTO> result = productService.findAll(PageRequest.of(0, 10));
+        Assertions.assertFalse(result.isEmpty());
+        Assertions.assertEquals(0, result.getNumber());
+        Assertions.assertEquals(10, result.getSize());
+        Assertions.assertEquals(totalProducts, result.getTotalElements());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnEmptyPageWhenPageDoesNotExist() {
+        Page<ProductDTO> result = productService.findAll(PageRequest.of(50, 10));
+        Assertions.assertTrue(result.isEmpty());
+    }
+
+    @Test
+    public void findAllPagedShouldReturnSortedPageWhenSortByName() {
+        Page<ProductDTO> result = productService.findAll(PageRequest.of(0, 10, Sort.by("name")));
+        Assertions.assertEquals("Macbook Pro", result.getContent().getFirst().getName());
+        Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName());
+        Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());
     }
 }
