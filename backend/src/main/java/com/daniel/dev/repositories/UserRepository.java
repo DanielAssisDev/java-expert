416ad1602cb894e5052d2b143e9984fb2771067a
DanielAssisDev <danielassisdeveloper@tutamail.com>
2026-09-05 19:34:37 -0300

    Criando UserRepository

---
+++ backend/src/main/java/com/daniel/dev/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.daniel.dev.repositories;
+
+import com.daniel.dev.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
+
